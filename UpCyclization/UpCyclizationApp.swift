1b40872888d906f4c028bab6bf0e25aff35b04e6
SURYASEN <myselfsuryasen@gmail.com>
2024-01-15 14:08:03 +0530

    Initial Commit

---
+++ UpCyclization/UpCyclizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpCyclizationApp.swift
+//  UpCyclization
+//
+//  Created by Suryasen on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct UpCyclizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
